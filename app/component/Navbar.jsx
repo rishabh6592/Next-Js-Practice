c8f50b10d104d46aeccabd91b02866e2405a9b3c
rishabh6592 <129681143+rishabh6592@users.noreply.github.com>
2024-06-19 18:10:03 +0530

    home pages

--- app/component/Navbar.jsx
+++ app/component/Navbar.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className=" w-full flex items-center justify-between bg-red-500 text-white px-5 py-2 w-full ">
+    <div className=" w-full flex items-center justify-between bg-green-600 text-white px-5 py-2 w-full ">
       <h2 className="text-5xl">Next-js</h2>
       <div className="flex gap-5 text-3xl">
         <Link href="/">Home</Link>
